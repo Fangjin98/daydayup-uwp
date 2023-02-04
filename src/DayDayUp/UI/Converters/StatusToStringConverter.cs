17c190087847d8dd47404e23eacbaf0f070759d1
Joey <fangjin98@mail.ustc.edu.cn>
2023-02-04 23:11:01 +0800

    can translate status

--- src/DayDayUp/UI/Converters/StatusToStringConverter.cs
+++ src/DayDayUp/UI/Converters/StatusToStringConverter.cs
@@ -1,19 +1,18 @@
 ﻿using DayDayUp.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Data;
 
 namespace DayDayUp.Helpers
 {
-    public class StatusToStringConverter : IValueConverter
+    public sealed class StatusToStringConverter : IValueConverter
     {
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var status = (value as TodoStatus?);
-            return status == TodoStatus.Doing ? "Doing" : "Pause";
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+
+            return status == TodoStatus.Doing ? loader.GetString("DoingStatus"): loader.GetString("PauseStatus");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
