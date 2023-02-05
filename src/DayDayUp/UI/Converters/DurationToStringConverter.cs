8b942ee753a3d9a77930477955068c5b00d79183
Joey <fangjin98@mail.ustc.edu.cn>
2023-02-05 21:39:47 +0800

    optimize duration display

--- src/DayDayUp/UI/Converters/DurationToStringConverter.cs
+++ src/DayDayUp/UI/Converters/DurationToStringConverter.cs
@@ -8,8 +8,16 @@ namespace DayDayUp.Helpers
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var number = (value as int?);
-            TimeSpan result = TimeSpan.FromMinutes((double) number);
-            return result.ToString("dd':'hh':'mm");
+            var result = TimeSpan.FromMinutes((double)number);
+            var loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+            var durationString = "";
+
+            if(result.TotalMinutes ==0) { return loader.GetString("None"); }
+
+            if(result.Days > 0) { durationString += (result.Days.ToString() + " " + loader.GetString("Day")+" "); }
+            if(result.Hours > 0) { durationString += (result.Hours.ToString() + " " + loader.GetString("Hour")+" "); }
+            if(result.Minutes > 0) { durationString += (result.Minutes.ToString() + " " + loader.GetString("Minute")); }
+            return durationString;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
