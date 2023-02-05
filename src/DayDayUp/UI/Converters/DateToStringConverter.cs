627f16379a150600f8ded41806c6e47d58dff07b
Joey <fangjin98@mail.ustc.edu.cn>
2023-02-05 15:37:11 +0800

    finish duration and date translation

--- src/DayDayUp/UI/Converters/DateToStringConverter.cs
+++ src/DayDayUp/UI/Converters/DateToStringConverter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Windows.UI.Xaml.Data;
 
 namespace DayDayUp.Helpers
@@ -11,10 +10,10 @@ namespace DayDayUp.Helpers
             if (value == null) { return null; }
             var date = (DateTime) value;
             var diffHour = (DateTime.Now - date).TotalHours;
-            if (diffHour<(24-date.Hour)) { return date.ToString("t"); }  // today
-            else if (diffHour< (48-date.Hour)) { return "Yesterday"; } // yesterday
+            if (diffHour < (24-date.Hour)) { return date.ToString("t"); }  // today
+            else if (diffHour < (24*7 - date.Hour)) { return date.ToString("dddd"); } // this week
             else if (date.Year == DateTime.Today.Year) { return date.ToString("M"); } // this year
-            else { return date.ToString("d"); }
+            else { return date.ToString("F"); }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
